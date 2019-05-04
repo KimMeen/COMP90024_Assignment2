23da4e5ff9dfd22c999a5adef6cfc8427cd30cb7
zack0518 <774726628@qq.com>
2019-05-04 22:21:19 +1000

    update

--- Server/Server.py
+++ Server/Server.py
@@ -5,16 +5,15 @@ import json
 import urllib.request
 import MapRegion
 app = Flask(__name__)
-
 class Db:
 
     """
     Initial the couchDB with the given name
     """
     def __init__(self, dbname):
-        user = "ziang"
-        password = "123456"
-        self.couchserver = couchdb.Server("http://%s:%s@127.0.0.1:5984/" % (user, password))
+        user = "admin"
+        password = "admin"
+        self.couchserver = couchdb.Server("http://%s:%s@172.26.37.207:5984/" % (user, password))
         self.selectDb(dbname)
     """
     Select db by a given name
@@ -31,7 +30,7 @@ class Db:
         list = []
         for doc in self.db:
             list.append(self.db[doc])
-        return list
+        return self.db
 
     """
     add a data entry to a database
@@ -39,25 +38,37 @@ class Db:
     def writeDoc2DB(self, data):
         self.db.save(data)
 
+    def loadWebData(self):
+        with open('aus_regions.json') as f:
+            regions = json.load(f)
+        for r in regions['features']:
+            value = self.getPlaceValue(regions, r['properties']['Name'])
+            r['sentiment_score'] = value
+        with open("../WebPage/regionsData.json", "w") as out:
+            out.write("var statistic = " + json.dumps(regions))
+
+    # get the related value of the place
+    def getPlaceValue(self, regions, regionName):
+        if regions is None:
+            return None
+        for i in regions['rows']:
+            if i['key'] == regionName:
+                return i['value']
+        return None
+
 
 @app.route('/')
 def home():
     return "Connected To Server"
 
-
-@app.route('/<string:db_name>', methods=["GET"])
+@app.route('/db/<string:db_name>', methods=["GET"])
 def getAllFromDB(db_name):
     db = Db(db_name)
     data = {"reply": db.getAllDoc()}
     res = flask.make_response(flask.jsonify(data))
-    res = formHeader(res)
-    return res
-
-# form a get header
-def formHeader(res):
     res.headers['Access-Control-Allow-Origin'] = '*'
     res.headers['Access-Control-Allow-Methods'] = 'GET'
-    res.headers['Access-Control-Allbow-Headers'] = 'x-requested-with,content-type'
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
     return res
 
 @app.route('/sentiment_average', methods=["GET"])
@@ -72,4 +83,5 @@ def period_view():
     return res
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    db = Db('tweets')
+    app.run(host='0.0.0.0', debug=True)
--- WebPage/js/dataRequest.js
+++ WebPage/js/dataRequest.js
@@ -1,7 +1,8 @@
 function getMapData(){
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", 'http://127.0.0.1:5000/comp90024', false);
+    xmlHttp.open("GET", 'http://127.0.0.1:5000/db/tweets', false);
     xmlHttp.send();
     var obj = JSON.parse(xmlHttp.responseText);
     return obj;
 };
+
