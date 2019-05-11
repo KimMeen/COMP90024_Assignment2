c88e45bafbf953d51e9d2fd7b7e252922943ebf0
zack0518 <774726628@qq.com>
2019-05-11 22:34:18 +1000

    delete useless

--- WebPage/js/dataRequest.js
+++
@@ -1,8 +0,0 @@
-function getMapData(){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", 'http://127.0.0.1:5000/db/tweets', false);
-    xmlHttp.send();
-    var obj = JSON.parse(xmlHttp.responseText);
-    return obj;
-};
-
